0705a956772867cc9495b16d28753cc5369e7bd4
Echogene <echogene.alpha@gmail.com>
2013-08-04 22:06:45 +0100

    Work in progress: fixed set identity function factory

--- src/logic/function/set/identity/SetIdentityFunctionFactory.java
+++ src/logic/function/set/identity/SetIdentityFunctionFactory.java
@@ -2,12 +2,23 @@ package logic.function.set.identity;
 
 import logic.Nameable;
 import logic.function.Function;
+import logic.function.factory.construction.Constructor;
 import logic.function.factory.construction.ValidatorAndConstructor;
+import logic.function.factory.validation.Validator;
+import logic.function.factory.validation.WordAtom;
+import logic.function.factory.validation.group.validators.FunctionOrVariableValidator;
+import logic.function.factory.validation.results.FunctionResult;
+import logic.function.factory.validation.results.StringResult;
+import logic.function.factory.validation.results.ValidationResult;
+import logic.function.set.SetFunction;
 import logic.function.set.SetFunctionFactory;
 import logic.set.Set;
-import sun.reflect.generics.reflectiveObjects.NotImplementedException;
 
+import java.util.Arrays;
 import java.util.List;
+
+import static logic.function.factory.validation.GroupValidatorWithNumber.Number.ONE;
+import static logic.function.set.identity.SetIdentityFunction.SET_IDENTITY_NAME;
 
 /**
  * @author Steven Weston
@@ -18,6 +29,46 @@ public class SetIdentityFunctionFactory<T extends Nameable> extends SetFunctionF
 	}
 
 	private static <T extends Nameable> List<ValidatorAndConstructor<Function<T, Set<T>>>> getConstructors() {
-		throw new NotImplementedException();
+		Validator validatorWithoutId = new Validator();
+		validatorWithoutId.addValidator(new FunctionOrVariableValidator(SetFunction.class), ONE);
+		ValidatorAndConstructor<Function<T, Set<T>>> constructorWithoutId = new ValidatorAndConstructor<>(
+				validatorWithoutId,
+				new ConstructorWithoutId<T>()
+		);
+		Validator validatorWithId = new Validator();
+		validatorWithId.addValidator(new WordAtom(SET_IDENTITY_NAME), ONE);
+		validatorWithId.addValidator(new FunctionOrVariableValidator(SetFunction.class), ONE);
+		ValidatorAndConstructor<Function<T, Set<T>>> constructorWithId = new ValidatorAndConstructor<>(
+				validatorWithId,
+				new ConstructorWithId<T>()
+		);
+		return Arrays.asList(constructorWithoutId, constructorWithId);
+	}
+	private static class ConstructorWithoutId<T extends Nameable> implements Constructor<Function<T, Set<T>>> {
+		@Override
+		public Function<T, Set<T>> construct(List<ValidationResult> results) {
+			ValidationResult validationResult = results.get(1);
+			if (validationResult instanceof StringResult) {
+				StringResult result = (StringResult) validationResult;
+				return new SetIdentityFunction<>(result.getString());
+			} else {
+				FunctionResult result = (FunctionResult) validationResult;
+				return new SetIdentityFunction<>((SetFunction<T>) result.getFunction());
+			}
+		}
+	}
+
+	private static class ConstructorWithId<T extends Nameable> implements Constructor<Function<T, Set<T>>> {
+		@Override
+		public Function<T, Set<T>> construct(List<ValidationResult> results) {
+			ValidationResult validationResult = results.get(2);
+			if (validationResult instanceof StringResult) {
+				StringResult result = (StringResult) validationResult;
+				return new SetIdentityFunction<>(result.getString());
+			} else {
+				FunctionResult result = (FunctionResult) validationResult;
+				return new SetIdentityFunction<>((SetFunction<T>) result.getFunction());
+			}
+		}
 	}
 }
