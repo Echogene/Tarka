30191fed79b58e62a0a3f9217d394efaea97ad12
Echogene <echogene.alpha@gmail.com>
2013-11-14 18:33:48 +0000

    Rename parameter

--- src/logic/function/factory/validation/function/FunctionValidator.java
+++ src/logic/function/factory/validation/function/FunctionValidator.java
@@ -11,5 +11,5 @@ import java.util.List;
  */
 public interface FunctionValidator {
 
-	MapToErrors<Function<?, ?>> validate(List<Function<?, ?>> tokens) throws TokenValidationException;
+	MapToErrors<Function<?, ?>> validate(List<Function<?, ?>> functions) throws TokenValidationException;
 }
