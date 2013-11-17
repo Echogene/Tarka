b73c573b680cc72baecaff7988a2befa83320436
Echogene <echogene.alpha@gmail.com>
2013-11-17 13:53:10 +0000

    Add BinaryUnionFactory to StandardReader

--- src/logic/function/set/union/BinaryUnionFactory.java
+++ src/logic/function/set/union/BinaryUnionFactory.java
@@ -14,7 +14,7 @@ import java.util.List;
  */
 public class BinaryUnionFactory<T extends Nameable> extends AbstractUnionFactory<T> {
 
-	protected BinaryUnionFactory() {
+	public BinaryUnionFactory() {
 		super(getCheckers());
 	}
 
