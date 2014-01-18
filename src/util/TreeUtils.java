b4eb74d1dbd6ee0f52fa425f042afb3b09c9a2f4
Echogene <echogene.alpha@gmail.com>
2014-01-18 11:36:53 +0000

    Added some pretty printing for trees.  The code isn't pretty, but the output is

--- src/util/TreeUtils.java
+++ src/util/TreeUtils.java
@@ -5,6 +5,7 @@ import reading.parsing.ParseTreeNode;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.function.Function;
 
 /**
  * @author Steven Weston
@@ -31,5 +32,57 @@ public class TreeUtils {
 			output.add(nodes.get(0).getFather());
 		}
 		return output;
+	}
+
+	public static <T> String prettyPrint(
+			List<T> list,
+			Function<T, String> presentationFunction,
+			Function<T, List<T>> childrenFunction,
+			int indentation
+	) {
+		StringBuilder output = new StringBuilder();
+		for (T parent : list) {
+			output.append(presentationFunction.apply(parent));
+			output.append("\n");
+			List<T> children = childrenFunction.apply(parent);
+			if (children == null || children.isEmpty()) {
+				continue;
+			}
+			String childString = prettyPrint(children, presentationFunction, childrenFunction, indentation);
+			String[] split = childString.split("\n");
+			for (int i = 0; i < split.length; i++) {
+				String s = split[i];
+				String trimmed = s.trim();
+				Boolean isAnotherChild = false;
+				for (int j = i + 1; j < split.length; j++) {
+					String t = split[j].trim();
+					if (!t.startsWith("└") && !t.startsWith("├") && !t.startsWith("│")) {
+						isAnotherChild = true;
+					}
+				}
+				if (trimmed.startsWith("└") || trimmed.startsWith("├") || trimmed.startsWith("│")) {
+					if (isAnotherChild) {
+						output.append("│");
+					} else {
+						output.append(" ");
+					}
+					for (int j = 0; j < indentation; j++) {
+						output.append(" ");
+					}
+				} else {
+					if (isAnotherChild) {
+						output.append("├");
+					} else {
+						output.append("└");
+					}
+					for (int j = 0; j < indentation; j++) {
+						output.append("─");
+					}
+				}
+				output.append(s);
+				output.append("\n");
+			}
+		}
+		return output.toString();
 	}
 }
