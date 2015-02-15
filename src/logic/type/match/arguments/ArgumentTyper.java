746eff8ed0881ccc49b038a2dedcc98db364d23f
Echogene <echogene.alpha@gmail.com>
2015-02-15 17:04:29 +0000

    Add some documentation

--- src/logic/type/match/arguments/ArgumentTyper.java
+++ src/logic/type/match/arguments/ArgumentTyper.java
@@ -7,5 +7,11 @@ import logic.type.match.subfunction.SubFunctionsTypedMatch;
  */
 public interface ArgumentTyper {
 
+	/**
+	 * Restrict the argument types of the given match to the ones possible as defined by this class.
+	 * @param match the match of which to restrict the argument types
+	 * @return an extension of the match that has restricted the arguments' types
+	 * @throws ArgumentTyperException if an argument has no possible types
+	 */
 	ArgumentsTypedMatch restrictArgumentTypes(SubFunctionsTypedMatch match) throws ArgumentTyperException;
 }
